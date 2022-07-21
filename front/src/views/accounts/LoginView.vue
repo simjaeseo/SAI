253c28611f8a8d577d3aa706c42e169e00904d8c
Jisu Kim <kimvely0422@gmail.com>
2022-07-21 17:52:29 +0900

    chore: login page css

--- front/src/views/accounts/LoginView.vue
+++ front/src/views/accounts/LoginView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
+  <div id="wrap1" class="container">
+    <div id="wrap2" class="row">
+      <!-- 로그인이미지 -->
+      <div id="login-image1" class="d-none d-md-block col-md-6">
+      </div>
+      <!-- 로그인폼 -->
+      <div id="login-form" class="col-sm-12 col-md-6">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,5 +18,19 @@ export default {
 </script>
 
 <style>
-
+#wrap1 {
+  border: 1px solid black;
+  height: 80vh;
+  width: 80%;
+  margin: 0 auto;
+}
+#wrap2 {
+  height: 100%;
+}
+#login-image1 {
+  background-color: red;
+}
+#login-form {
+  background-color: green;
+}
 </style>
