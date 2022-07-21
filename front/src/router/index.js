38b3a25c670a1a9774215d3d07f35436acebae03
cih831 <choi_026@naver.com>
2022-07-21 13:31:34 +0900

    build: create vue project

---
+++ front/src/router/index.js
@@ -0,0 +1,13 @@
+import accounts from './accounts';
+import interview from './interview';
+import schedule from './schedule';
+
+const router = ({
+  modules: {
+    accounts,
+    interview,
+    schedule,
+  },
+});
+
+export default router;
---
+++ front/src/store/index.js
@@ -0,0 +1,13 @@
+import accounts from '@/store/modules/accounts';
+import interview from '@/store/modules/interview';
+import schedule from '@/store/modules/schedule';
+
+const store = ({
+  modules: {
+    accounts,
+    interview,
+    schedule,
+  },
+});
+
+export default store;
