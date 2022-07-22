8a655d0de0cc4531e558fe2cb38a0af59b2ab0f8
hanggeee <yoonhangg@gmail.com>
2022-07-22 13:53:41 +0900

    build(frontend): SettingCheck.vue

--- front/src/views/interview/InterviewView.vue
+++ front/src/views/interview/InterviewView.vue
@@ -1,18 +1,20 @@
 <template>
   <progress-bar></progress-bar>
   <mode-select></mode-select>
-
+  <setting-check></setting-check>
 </template>
 
 <script>
 import ProgressBar from '@/views/interview/components/ProgressBar.vue';
 import ModeSelect from '@/views/interview/components/ModeSelect.vue';
+import SettingCheck from '@/views/interview/components/SettingCheck.vue';
 
 export default {
   // name: 'InterviewView',
   components: {
     ProgressBar,
     ModeSelect,
+    SettingCheck,
   },
 };
 </script>
---
+++ front/src/views/interview/components/SettingCheck.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <p>카메라, 마이크를 체크합니다.</p>
+    <div class="box"></div>
+    <button class="btn btn-primary">시작하기</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 1600px;
+  padding: 0;
+}
+p {
+  font-size: 48px;
+}
+.box {
+  display: flex;
+  justify-content: center;
+  border: 1px solid black;
+  background: grey;
+  width: 1600px;
+  height: 900px;
+  margin-bottom: 1rem;
+}
+
+.btn {
+  font-size: 24px;
+}
+</style>
