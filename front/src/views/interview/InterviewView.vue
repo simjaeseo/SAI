12c238cf9246befb80b1fbbe7e8454ed3ff11c81
hanggeee <yoonhangg@gmail.com>
2022-07-22 11:59:45 +0900

    build(frontend): InterviewView

---
+++ front/src/views/interview/InterviewView.vue
@@ -0,0 +1,22 @@
+<template>
+  <progress-bar></progress-bar>
+  <mode-select></mode-select>
+
+</template>
+
+<script>
+import ProgressBar from '@/views/interview/components/ProgressBar.vue';
+import ModeSelect from '@/views/interview/components/ModeSelect.vue';
+
+export default {
+  // name: 'InterviewView',
+  components: {
+    ProgressBar,
+    ModeSelect,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/src/views/interview/components/ModeSelect.vue
@@ -0,0 +1,61 @@
+<template>
+  <main class="page-content">
+    <div class="card">
+        <div class="content">
+            <h2 class="title">나 혼자 연습</h2>
+            <p class="copy"></p><button class="btn">시작하기</button>
+        </div>
+    </div>
+    <div class="card">
+        <div class="content">
+            <h2 class="title">컨설턴트님과 1:1 면접</h2>
+            <p class="copy"></p>
+            <div class="d-flex flex-column align-items-center">
+              <button class="btn">1:1 면접 신청</button>
+              <button class="btn">--월 --일 --시 --분<br>1:1 면접 시작하기</button>
+            </div>
+        </div>
+    </div>
+
+  </main>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+.page-content {
+  display: flex;
+  justify-content: space-between;
+  grid-gap: 1rem;
+  padding: 1rem;
+  max-width: 1024px;
+  margin: 0 auto;
+  font-family: var(--font-sans);
+}
+
+.card {
+  position: relative;
+  display: flex;
+  padding: 1rem;
+  width: 400px;
+  height: 600px;
+  color: whitesmoke;
+  background-color: whitesmoke;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1), 0 2px 2px rgba(0, 0, 0, 0.1), 0 4px 4px
+  rgba(0, 0, 0, 0.1), 0 8px 8px rgba(0, 0, 0, 0.1), 0 16px 16px rgba(0, 0, 0, 0.1);
+}
+
+.title {
+  color: black;
+  font-size: 48px;
+}
+.btn {
+  color: white;
+  background-color: rgb(33, 150, 243);
+}
+</style>
