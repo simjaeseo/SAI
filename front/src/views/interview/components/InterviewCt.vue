6f96e7caf12ce52d752b019d8bd87df6193dfad7
hanggeee <yoonhangg@gmail.com>
2022-07-22 16:50:59 +0900

    build(frontend): InterviewCt.vue

--- front/src/views/interview/components/InterviewCt.vue
+++ front/src/views/interview/components/InterviewCt.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="container">
-    <!-- <button class="btn btn-primary">면접 시작</button> -->
-    <div class="d-flex justify-content-end" id="x">
-      <button class="btn btn-dark">종료</button>
-    </div>
-    <div>
-      <div class="questionbox">자기소개 부탁드립니다.</div>
-      <div class="interviewer">
-        <div class="interviewee">interviewee</div>
+    <div class="box">
+      <!-- <button class="btn btn-primary">면접 시작</button> -->
+      <div class="d-flex justify-content-end" id="x">
+        <button class="btn btn-dark">종료</button>
+      </div>
+      <div>
+        <div class="questionbox">자기소개 부탁드립니다.</div>
+        <div class="interviewer">
+          <div class="interviewee">interviewee</div>
+        </div>
       </div>
     </div>
+    <div class="d-flex justify-content-end">
+      <button class="btn btn-primary">답변 완료</button>
+      <button class="btn btn-primary">면접 종료 및 피드백 시작</button>
+    </div>
   </div>
-  <button class="btn btn-primary">답변 완료</button>
+
 </template>
 
 <script>
@@ -26,12 +32,15 @@ export default {
   /* display: flex;
   justify-content: center;
   align-items: center; */
-  border: 1px solid black;
-  width: 1600px;
-  height: 900px;
+
   padding: 0;
 }
 
+.box {
+  border: 1px solid black;
+  width: 1600px;
+  height: 900px;
+}
 .interviewer{
   display: flex;
   justify-content: center;
@@ -62,6 +71,7 @@ export default {
   line-height: 100px;
   text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
 }
+
 .btn {
   margin: 0.3rem;
 }
