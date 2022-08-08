1b50a5054829b5c3a1ae658c4f712e9bd0b7eae7
DESKTOP-KVCQHCD\multicampus <razr1919@gmail.com>
2022-08-08 16:14:36 +0900

    feat: custom interview question

--- backend/src/main/java/com/ssafy/sai/domain/interview/repository/CustomQuestionRepository.java
+++ backend/src/main/java/com/ssafy/sai/domain/interview/repository/CustomQuestionRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CustomQuestionRepository extends JpaRepository<CustomInterviewQuestion, Long> {
 
     List<CustomInterviewQuestion> findByMember(Member member);
+
+    List<CustomInterviewQuestion> findQuestionsByMemberId(Long id);
+
+    List<CustomInterviewQuestion> findAllByMember(Member member);
+
+
 }
--- backend/src/main/java/com/ssafy/sai/domain/interview/service/InterviewService.java
+++ backend/src/main/java/com/ssafy/sai/domain/interview/service/InterviewService.java
@@ -2,15 +2,16 @@ package com.ssafy.sai.domain.interview.service;
 
 import com.ssafy.sai.domain.interview.domain.CustomInterviewQuestion;
 import com.ssafy.sai.domain.interview.domain.InterviewQuestion;
-import com.ssafy.sai.domain.interview.dto.CustomQuestionDto;
-import com.ssafy.sai.domain.interview.dto.questionRequest.QuestionRequest;
+import com.ssafy.sai.domain.interview.dto.request.CustomQuestionRequest;
 import com.ssafy.sai.domain.member.domain.Member;
 import com.ssafy.sai.domain.interview.repository.CustomQuestionRepository;
 import com.ssafy.sai.domain.interview.repository.QuestionRepository;
+import com.ssafy.sai.domain.member.exception.MemberException;
+import com.ssafy.sai.domain.member.exception.MemberExceptionType;
+import com.ssafy.sai.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +25,7 @@ public class InterviewService {
     private final QuestionRepository questionRepository;
     private final CustomQuestionRepository customQuestionRepository;
 
+    private final MemberRepository memberRepository;
 
     @Transactional
     public Optional<InterviewQuestion> getQuestion(Long id){
@@ -39,30 +41,26 @@ public class InterviewService {
 
     @Transactional
     public InterviewQuestion getRandomQuestion(String questionType, String questionDetailType){
-
-
         List<InterviewQuestion> findQuestionList = questionRepository.findAllByQuestionTypeAndQuestionDetailType(questionType,questionDetailType);
-
 //        List<InterviewQuestion> questions = questionRepository.findAllByQuestionTypeAndQuestionDetailType(request);
         return findQuestionList.get((int)Math.random()*(findQuestionList.size()));
     }
 
 
     @Transactional
-    public List<CustomInterviewQuestion> getCustomInterviewQuestionList(Member member){
-
-        List<CustomInterviewQuestion> customInterviewQuestionList = customQuestionRepository.findByMember(member);
+    public List<CustomInterviewQuestion> getCustomInterviewQuestionList(CustomQuestionRequest request) throws MemberException {
+        Member findMember = memberRepository.findById(request.getMemberId()).orElseThrow(() -> new MemberException(MemberExceptionType.NOT_FOUND_MEMBER));
+        List<CustomInterviewQuestion> customInterviewQuestionList = customQuestionRepository.findQuestionsByMemberId(findMember.getId());
         return customInterviewQuestionList;
     }
 
-//    @Transactional
-//    public Optional<CustomInterviewQuestion> createCustomInterviewQuestion(CustomQuestionDto customQuestionDto){
-//            CustomInterviewQuestion customInterviewQuestion = new CustomInterviewQuestion();
-//            CustomInterviewQuestion.setQuestion(customInterviewQuestion.getQuestion());
-//            customQuestionRepository.save(customInterviewQuestion);
-//
-//
-//    }
+    @Transactional
+    public void createCustomInterviewQuestion(CustomQuestionRequest request) throws MemberException {
+        Member findMember = memberRepository.findById(request.getMemberId()).orElseThrow(() -> new MemberException(MemberExceptionType.NOT_FOUND_MEMBER));
+        CustomInterviewQuestion question = new CustomInterviewQuestion(request.getQuestion(), findMember);
+        customQuestionRepository.save(question);
+
+    }
 
     @Transactional
     public void deleteCustomInterviewQuestion(Long id){
