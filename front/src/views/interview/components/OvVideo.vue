95c313cfc8c4aa871b6ac06a088f5cc1a054b2ad
cih831 <choi83195@gmail.com>
2022-08-14 00:50:49 +0900

    chore: git conflict error

--- front/src/views/interview/components/OvVideo.vue
+++ front/src/views/interview/components/OvVideo.vue
@@ -8,10 +8,7 @@
 </template>
 
 <script>
-// import { store } from 'vuex';
 import { onMounted } from 'vue';
-// import { onMounted, reactive, computed } from 'vue';
-// import * as faceapi from 'face-api.js';
 import * as faceapi from '../../../assets/face-api.min';
 
 export default {
@@ -22,62 +19,6 @@ export default {
   },
 
   setup() {
-    // const state = reactive({
-    //   detections: null,
-    //   myEmotion: '',
-    //   webCamWidth: 0,
-    //   webCamHeight: 0,
-    //   hide: false,
-    //   timerId: 0,
-    //   emotions: {
-    //     angry: 0,
-    //     disgusted: 0,
-    //     fearful: 0,
-    //     happy: 0,
-    //     neutral: 0,
-    //     sad: 0,
-    //     surprised: 0,
-    //   },
-    // });
-
-    // const startExpressDetection = async () => {
-    //   console.log('1');
-    //   state.timerId = setInterval(async () => {
-    //     console.log('2');
-    //     console.log(video);
-    //     eslint-disable-next-line
-    //     state.detections = await faceapi.detectSingleFace(video.value, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
-    //     console.log('3');
-    //     if (state.detections) {
-    //       console.log('4');
-    //       console.log('4');
-    //       let maxVal = 0;
-    //       let maxEmotion = '';
-    //       // eslint-disable-next-line
-    //       for (let emotion in state.detections.expressions) {
-    //         console.log('55555');
-    //         if (state.detections.expressions[emotion] > maxVal) {
-    //           maxVal = state.detections.expressions[emotion];
-    //           maxEmotion = emotion;
-    //         }
-    //       }
-    //       state.emotions[maxEmotion] += 1;
-    //       console.log(state.emotions);
-    //     }
-    //   }, 250);
-    // };
-    // Promise.all([
-    //   faceapi.nets.tinyFaceDetector.load('./models'),
-    //   // faceapi.nets.faceLandmark68Net.load('./models'),
-    //   // faceapi.nets.faceRecognitionNet.load('./models'),
-    //   faceapi.nets.faceExpressionNet.load('./models'),
-    //   // faceapi.nets.tinyYolov2.loadFromDisk('./models'),
-    //   // faceapi.nets.tinyFaceDetector.loadFromUri(faceDetector),
-    //   // faceapi.nets.faceLandmark68Net.loadFromUri(faceLandmark),
-    //   // faceapi.nets.faceRecognitionNet.loadFromUri(faceRecognition),
-    //   // faceapi.nets.faceExpressionNet.loadFromUri(faceExpression),
-    // // ]);
-    // ])
     const MODEL_URL = '/models';
     Promise.all([
       faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
@@ -86,7 +27,6 @@ export default {
       faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
     ]);
 
-    // window.onload = function () {
     onMounted(() => {
       const video = document.getElementById('video');
       video.addEventListener('play', () => {
@@ -98,28 +38,9 @@ export default {
           // eslint-disable-next-line
           const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
           console.log(detections[0].expressions);
-          // const resizedDetections = faceapi.resizeResults(detections, displaySize);
-          // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-          // faceapi.draw.drawDetections(canvas, resizedDetections);
-          // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-          // faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
         }, 100);
       });
     });
-
-    // startExpressDetection();
-
-    onMounted(async () => {
-      // const video = document.getElementById('video');
-
-      // // await this.streamManager.addVideoElement(this.$el);
-      // // console.log(faceapi);
-      // // await faceapi.nets.tinyFaceDetector.load('./models');
-      // // await faceapi.nets.faceExpressionNet.load('./models');
-      // await startExpressDetection();
-    });
-    return {
-    };
   },
 
   mounted() {
