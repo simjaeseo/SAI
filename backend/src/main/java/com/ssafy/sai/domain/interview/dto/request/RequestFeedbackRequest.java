46c93f190ffb73e3ee0f08fcbe1b62f3924b2917
simjaeseo <delpho@naver.com>
2022-08-13 19:11:55 +0900

    feat: 면접 정보 불러오기 api 및 피드백 보내기 api

---
+++ backend/src/main/java/com/ssafy/sai/domain/interview/dto/request/RequestFeedbackRequest.java
@@ -0,0 +1,10 @@
+package com.ssafy.sai.domain.interview.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RequestFeedbackRequest {
+
+    private Long interviewInfoId;
+    private Long consultantId;
+}
