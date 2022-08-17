12d0dfe123fe6f704eb1c2ed256c6ffaea1cb8a2
gitjisu <kimvely0422@gmail.com>
2022-08-18 03:40:07 +0900

    feat: question style

--- front/src/views/interview/InterviewRoomView.vue
+++ front/src/views/interview/InterviewRoomView.vue
@@ -122,19 +122,11 @@
       <div id="session" v-if="session">
         <div id="main-video">
           <div id="video-box">
-            <div v-if="question.length" style="display:inline;">
-              <span v-if="!isFinished"> 질문 : </span>
-              <span>{{question}}</span>
-            </div>
-            <div>
-            </div>
             <user-video :stream-manager="mainStreamManager" @emotionRatioCount="emotionRatioCount"/>
-            <div id="video-text" v-if="isAnimationStart">
-              <p id="video-start"> 모의 면접을 시작합니다.</p>
+            <div id="video-text" v-if="isRecording">
+              <p id="video-start"> {{question}} </p>
               <p id="video-start"> 질문에 답변해주세요. </p>
               <div class="cd-number-wrapper">
-                <!-- <span class="cd-number-five">5</span>
-                <span class="cd-number-four">4</span> -->
                 <span class="cd-number-three">3</span>
                 <span class="cd-number-two">2</span>
                 <span class="cd-number-one">1</span>
